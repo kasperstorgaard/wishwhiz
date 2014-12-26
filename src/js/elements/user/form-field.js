1b6deb860b64f20bdd35d8d3ee16c35a6ab4a040
kasperstorgaard <kst@codehouse.com>
2014-12-26 22:37:07 +0100

    error messages for fields

--- src/js/elements/user/form-field.js
+++ src/js/elements/user/form-field.js
@@ -6,12 +6,24 @@ var Field = React.createClass({displayName: "Field",
     var val = this.refs.input.getDOMNode().value || '';
     this.props.update(this.props.id, val);
   },
+  getValidationMessage: function(error) {
+    if(!error.message){
+      return null;
+    }
+    return (React.createElement("span", {className: "help-inline"}, error.message));
+  },
   render: function() {
+    var error = this.props.validationError;
+    var hasError = _.size(error) != 0;
+    var validationMessage = !hasError ? null : this.getValidationMessage(error);
+
     return (
-      React.createElement("div", {className: "form-group" + (_.size(this.props.validationError) == 0 ? "" : " has-error")}, 
+      React.createElement("div", {className: "form-group" + (hasError ? " has-error" : "")}, 
         React.createElement("label", {htmlFor: this.props.id, className: "col-lg-2 control-label"}, this.props.name), 
         React.createElement("div", {className: "col-lg-10"}, 
-          React.createElement("input", {type: this.props.type, className: "form-control", onBlur: this.handleBlur, ref: "input", id: this.props.id, placeholder: this.props.placeholder})
+          React.createElement("input", {type: this.props.type, className: "form-control", onBlur: this.handleBlur, ref: "input", 
+           id: this.props.id, placeholder: this.props.placeholder}), 
+           validationMessage
         )
       )
     );
--- src/js/elements/user/form-field.jsx
+++ src/js/elements/user/form-field.jsx
@@ -6,12 +6,24 @@ var Field = React.createClass({
     var val = this.refs.input.getDOMNode().value || '';
     this.props.update(this.props.id, val);
   },
+  getValidationMessage: function(error) {
+    if(!error.message){
+      return null;
+    }
+    return (<span className="help-inline">{error.message}</span>);
+  },
   render: function() {
+    var error = this.props.validationError;
+    var hasError = _.size(error) != 0;
+    var validationMessage = !hasError ? null : this.getValidationMessage(error);
+
     return (
-      <div className={"form-group" + (_.size(this.props.validationError) == 0 ? "" : " has-error")}>
+      <div className={"form-group" + (hasError ? " has-error" : "")}>
         <label htmlFor={this.props.id} className="col-lg-2 control-label">{this.props.name}</label>
         <div className="col-lg-10">
-          <input type={this.props.type} className="form-control" onBlur={this.handleBlur} ref="input" id={this.props.id} placeholder={this.props.placeholder} />
+          <input type={this.props.type} className="form-control" onBlur={this.handleBlur} ref="input"
+           id={this.props.id} placeholder={this.props.placeholder} />
+           {validationMessage}
         </div>
       </div>
     );
